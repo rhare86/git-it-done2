c542f60bc886dd3e6cf3147b398f789a4a5128d2
rhare86 <rhare86@gmail.com>
2022-07-20 16:59:12 -0500

    update request logic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,15 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    // format the github url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
+var response = fetch("https://api.github.com/users/octocat/repos");
+    console.log(response);
 
 getUserRepos();
