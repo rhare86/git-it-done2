9cfd1d431856313af6eda9f439cbc95436f06b63
rhare86 <rhare86@gmail.com>
2022-07-21 00:17:20 -0500

    end of 6.3

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,17 @@
+var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
+
+var displayWarning = function(repo) {
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -10,6 +23,11 @@ var getRepoIssues = function(repo) {
                 response.json().then(function(data) {
                     // pass response data to dom function
                     displayIssues(data);
+
+                    // check if api has paginated issues
+                    if (response.headers.get("Link")) {
+                        displayWarning(repo);
+                    }
                 });
             }
             else {
